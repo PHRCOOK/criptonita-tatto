6f202f86266f4f262323de3c935b977c92cafdb2
PHRCOOK <https://pabloelleproso@gmail.com>
2024-07-25 23:24:37 -0300

    update main

---
+++ client/src/components/home/home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../navbar/navbar.jsx"; // Asegúrate de que la ruta sea correcta
+import Footer from "../footer/footer.jsx";
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar />
+      <Footer />
+    </div>
+  );
+}
---
+++ client/src/components/navbar/navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div>
+      <button>Home</button>
+      <button>About</button>
+      <button>Services</button>
+      <button>Contact</button>
+      <button>Help</button>
+      <input type="text" placeholder="Search..." />
+      <button>Search</button>
+    </div>
+  );
+}
