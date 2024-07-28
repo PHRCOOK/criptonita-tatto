c251946b6f9ca6fb96a4f240e5117a1986ec20fc
PHRCOOK <https://pabloelleproso@gmail.com>
2024-07-27 21:07:13 -0300

    server eliminated

--- server/models/memberships/index.js
+++
@@ -1,9 +0,0 @@
-const { DataTypes } = require("sequelize");
-const sequelize = require("../../config-db/config-db");
-
-const Membership = sequelize.define("Membership", {
-  name: DataTypes.STRING,
-  benefits: DataTypes.TEXT,
-});
-
-module.exports = Membership;
